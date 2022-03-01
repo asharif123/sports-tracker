9a0d2875ddf4191b5c5b8a40f5695a6fba4f0b7b
Awad Sharif <awadsharif9@gmail.com>
2022-02-28 19:01:42 -0800

    added server.js file

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+const routes = require("./controllers");
+const sequelize = require("./config/connection");
+const PORT = process.env.PORT || 3001;
+
+  
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(routes);
+
+sequelize.sync({ force: true }).then(() => {
+  app.listen(PORT, () => console.log("Now listening"));
+});
