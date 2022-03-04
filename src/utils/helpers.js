9496a14fc2892b0ac601956f60c1082a7ee96e91
Awad Sharif <awadsharif9@gmail.com>
2022-03-03 18:51:08 -0800

    added User model

---
+++ src/utils/helpers.js
@@ -0,0 +1,13 @@
+export function validateLoginEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  }
+  
+  export function checkLoginPassword(input) {
+    const passw = /^[A-Za-z]\w{7,14}$/;
+    if (input.match(passw)) {
+      return true;
+    }
+    return false;
+  }
+  
