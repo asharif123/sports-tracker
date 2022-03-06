0f4e6967df9db4b5b1720c820c2aa39c1d86a6e0
Mason Newell <mason.newell13@gmail.com>
2022-03-06 13:13:26 -0800

    added buttons/logic for page numbers

--- sports-tracker-client/src/components/MediaGrid.js
+++ sports-tracker-client/src/components/MediaGrid.js
@@ -1,15 +1,17 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import MediaList from "./MediaList";
+import { Button, Container } from "react-bootstrap";
 
 function MediaGrid() {
   const [items, setItems] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [pageNumber, setPageNumber] = useState(1);
 
   useEffect(() => {
     const fetchAPI = async () => {
       const result = await axios.get(`https://sportscore1.p.rapidapi.com/medias`, {
-        params: { page: "1" },
+        params: { page: `${pageNumber}` },
         headers: {
           "x-rapidapi-host": "sportscore1.p.rapidapi.com",
           "x-rapidapi-key": process.env.REACT_APP_RAPID_API_KEY,
@@ -20,12 +22,31 @@ function MediaGrid() {
       setIsLoading(false);
     };
     fetchAPI();
-  }, []);
+  }, [pageNumber]);
+
+  function handlePageNumber(page) {
+    if (page <= 0) {
+      return;
+    }
+    setPageNumber(page);
+  }
 
   return (
-    <div>
+    <>
       <MediaList isLoading={isLoading} items={items} />
-    </div>
+      <Container className="d-flex align-items-center justify-content-center">
+        <div className="p-2">
+          <Button variant="info" size="lg" onClick={() => handlePageNumber(pageNumber - 1)}>
+            Previous
+          </Button>
+        </div>
+        <div className="p-2">
+          <Button variant="info" size="lg" onClick={() => handlePageNumber(pageNumber + 1)}>
+            Next
+          </Button>
+        </div>
+      </Container>
+    </>
   );
 }
 
