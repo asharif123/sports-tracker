03de0873439e9a20aa2f264d4dae5cb77327f549
Dustin Smith <dustinsmith1983@gmail.com>
2022-03-11 00:14:04 -0800

    added in live game score ticker

---
+++ sports-tracker-client/src/components/LiveScore/LiveScore.js
@@ -0,0 +1,82 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Ticker from 'react-ticker'
+import "bootstrap/dist/css/bootstrap.min.css";
+import { ListGroup } from "react-bootstrap";
+import "./LiveScore.css";
+
+// Create Live event Items
+const GetEvents = ({ scores }) => {
+
+  return scores ? (
+    <p className="tickerBar" style={{ whiteSpace: "nowrap" }}>
+        <ListGroup horizontal>
+        {scores.map((score) => (
+          <>
+            <ListGroup.Item className="border-0 tickerItems"><h1>|</h1></ListGroup.Item>
+            <ListGroup.Item className="border-0 tickerItems"><h3>{score.home_team.name_code}</h3></ListGroup.Item>
+            <ListGroup.Item className="border-0 tickerItems"><img height="50px" width="50px" src={score.home_team.logo} alt="" /></ListGroup.Item>
+            <ListGroup.Item className="border-0 tickerItems"><h4>-{score.home_score.current}</h4></ListGroup.Item>
+            <ListGroup.Item className="border-0 tickerItems"><h3>{score.away_team.name_code}</h3></ListGroup.Item>
+            <ListGroup.Item className="border-0 tickerItems"><img height="50px" width="50px" src={score.away_team.logo} alt="" /></ListGroup.Item>
+            <ListGroup.Item className="border-0 tickerItems"><h4>-{score.away_score.current}</h4></ListGroup.Item>
+            <ListGroup.Item className="border-0 tickerItems"><h1>|</h1></ListGroup.Item>
+          </>
+        ))}
+        </ListGroup>
+    </p>
+  ) : (
+    <p className="tickerBar" style={{ visibility: "hidden" }}>Placeholder</p>
+  );
+};
+
+function LiveScore() {
+  const [scores, setScores] = useState([]);
+
+  //for initial isLoading
+  useEffect(()=> {
+    axios.get(`https://sportscore1.p.rapidapi.com/events/live`, {
+      params: { 
+         page: 1,
+      },
+      headers: {
+         "x-rapidapi-host": "sportscore1.p.rapidapi.com",
+       "x-rapidapi-key": process.env.REACT_APP_RAPID_API_KEY,
+       },
+     }).then(({data}) => {
+       console.log(data.data);
+       setScores(data.data);
+      });
+  }, [])
+
+  // Timed call to refresh scores
+  useEffect(() => {
+    const timeer = setTimeout(()=> {
+      //debounced effect
+      async function fetchAPI() {
+        const result = await axios.get(`https://sportscore1.p.rapidapi.com/events/live`, {
+         params: { 
+            page: 1,
+         },
+         headers: {
+            "x-rapidapi-host": "sportscore1.p.rapidapi.com",
+          "x-rapidapi-key": process.env.REACT_APP_RAPID_API_KEY,
+          },
+        });
+        console.log(result.data.data);
+        setScores(result.data.data);
+      };
+      fetchAPI();
+    }, 600000);
+    return () => clearTimeout(timeer)
+    });
+  return (
+    <div className="ticker">
+      <Ticker  offset="run-in" speed={5}>
+        {() => <GetEvents scores={scores} />}
+      </Ticker>
+    </div>
+  );
+}
+
+export default LiveScore;
