bd6d4cc643b8ec813fc61f0549e842fb3233a269
Awad Sharif <awadsharif9@gmail.com>
2022-03-12 09:31:37 -0800

    quick fix
    ok
    
    ok
    
    ok
    
    ok

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require("express");
-const session = require('express-session');
+const session = require("express-session");
 
 const path = require("path");
-const cors = require("cors")
+const cors = require("cors");
 
 const app = express();
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 const routes = require("./controllers/api");
@@ -15,30 +18,39 @@ const sequelize = require("./config/connection");
 const PORT = process.env.PORT || 3001;
 
 const sess = {
-    secret: 'Super secret secret',
-      cookie: {
+  secret: "Super secret secret",
+  cookie: {
     // Stored in milliseconds (86400 === 1 day)
-        maxAge: 864000000000,
+    maxAge: 864000000000,
   },
 
   //starndard settings
-    resave: false,
-    saveUninitialized: false,
-  };
+  resave: false,
+  saveUninitialized: false,
+};
 
 app.use(session(sess));
-  
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
- app.use(express.static(path.join(__dirname, "./sports-tracker-client/public")));
- if(process.env.NODE_ENV === "production"){
-     app.use(express.static(path.join(__dirname, "./sports-tracker-client/build")));
- };
+if (process.env.NODE_ENV === "production") {
+  app.use(
+    express.static(path.join(__dirname, "./sports-tracker-client/build"))
+  );
+} else {
+  app.use(
+    express.static(path.join(__dirname, "./sports-tracker-client/public"))
+  );
+}
 // app.get("/test", (req, res) => {
 //   res.status(200).json('test')
 // })
 app.use(routes);
-app.get("*", (req,res)=> res.sendFile(path.join(__dirname, "./sports-tracker-client/public/index.html")));
+app.get("*", (req, res) =>
+  res.sendFile(
+    path.join(__dirname, "./sports-tracker-client/public/index.html")
+  )
+);
 sequelize.sync({ force: false }).then(() => {
-   app.listen(PORT, () => console.log("Now listening"));
+  app.listen(PORT, () => console.log("Now listening"));
 });
--- sports-tracker-client/src/pages/Logout.js
+++ sports-tracker-client/src/pages/Logout.js
@@ -1,28 +1,26 @@
-import {useEffect, React} from 'react';
-import { useNavigate } from 'react-router-dom';
-import {useContext} from "react";
-import {CountContext} from "../ContextProvider"; 
+import { useEffect, React } from "react";
+import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { CountContext } from "../ContextProvider";
 
-const apiEndpoint = process.env.NODE_ENV === "production" ? "https://someappname.herokuapp.com" : "http://localhost:3001";
+const apiEndpoint =
+  process.env.NODE_ENV === "production" ? "" : "http://localhost:3001";
 
 export const Logout = () => {
-    const { state, dispatch } = useContext(CountContext);
-    const navigate = useNavigate();
-    //if user is logged out and refreshes, user stays on login webpage
-    useEffect(async () => {
-        const response = await fetch(apiEndpoint + '/logout', {});
+  const { state, dispatch } = useContext(CountContext);
+  const navigate = useNavigate();
+  //if user is logged out and refreshes, user stays on login webpage
+  useEffect(async () => {
+    const response = await fetch(apiEndpoint + "/logout", {});
 
-        if (response.ok) {
-            dispatch({ type: 'LOGOUT' });
-            localStorage.setItem('loggedIn', 'false');
-            navigate('/')
-          }
-    },
-    []) 
+    if (response.ok) {
+      dispatch({ type: "LOGOUT" });
+      localStorage.setItem("loggedIn", "false");
+      navigate("/");
+    }
+  }, []);
 
-    return (
-        <div>Logout</div>
-    )
-}
+  return <div>Logout</div>;
+};
 
 export default Logout;
