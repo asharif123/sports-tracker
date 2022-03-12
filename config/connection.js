9bbdfac56e17bca6cc57654ce7007bfd0ed27ada
Awad Sharif <awadsharif9@gmail.com>
2022-03-12 09:23:30 -0800

    updated connection url

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
+const sequelize = process.env.JAWSDB_URL ? new Sequelize(process.env.JAWSDB_URL) new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
