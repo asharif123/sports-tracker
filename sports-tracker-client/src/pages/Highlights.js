8eff11f7c17ab605adf3c03584d66368e4a4d78a
Mason Newell <mason.newell13@gmail.com>
2022-03-19 09:36:39 -0700

    deleted unused import, formatted with prettier

--- sports-tracker-client/src/pages/Highlights.js
+++ sports-tracker-client/src/pages/Highlights.js
@@ -1,10 +1,10 @@
 import React from "react";
 import MediaGrid from "../components/MediaGrid";
-import  { useContext } from 'react';
-import { Navigate } from 'react-router-dom';
-import {CountContext} from "../ContextProvider";
+import { useContext } from "react";
+import { Navigate } from "react-router-dom";
+import { CountContext } from "../ContextProvider";
 function Highlights() {
-  const { state, dispatch } = useContext(CountContext);
+  const { state } = useContext(CountContext);
 
   return (
     <div>
