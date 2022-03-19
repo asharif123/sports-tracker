7dffd61fe6bf66af07cce648551c00b7886dbfe5
Mason Newell <mason.newell13@gmail.com>
2022-03-19 09:38:53 -0700

    formatted with prettier

--- sports-tracker-client/src/components/navbar/Navbar.js
+++ sports-tracker-client/src/components/navbar/Navbar.js
@@ -1,24 +1,20 @@
 import "./navbar.scss";
 
-export default function Navbar ({ menuOpen, setMenuOpen}) {
+export default function Navbar({ menuOpen, setMenuOpen }) {
   return (
     <>
-    <div className= {"navbar " + (menuOpen && "active")}>
-      <div className="wrapper">
-          <h1 className="title">
-            Sports Dome
-          </h1>
-        <div className="right">
-          <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
-            <span className="line1"></span>
-            <span className="line2"></span>
-            <span className="line3"></span>
+      <div className={"navbar " + (menuOpen && "active")}>
+        <div className="wrapper">
+          <h1 className="title">Sports Dome</h1>
+          <div className="right">
+            <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+              <span className="line1"></span>
+              <span className="line2"></span>
+              <span className="line3"></span>
+            </div>
           </div>
         </div>
       </div>
-      </div>
-     
     </>
-
-);
+  );
 }
