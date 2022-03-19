ab0d13094b4ed1e7e2fa47dc486ca3ef7b15a425
Mason Newell <mason.newell13@gmail.com>
2022-03-19 09:37:38 -0700

    deleted unused API fetch

--- sports-tracker-client/src/components/sportsOdds/Sports.js
+++ sports-tracker-client/src/components/sportsOdds/Sports.js
@@ -1,27 +1,10 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
 import { Container, Row } from "react-bootstrap";
 import SportsList from "./SportsList";
 
 function Sports() {
   const [isLoading, setIsLoading] = useState(true);
   const [items, setItems] = useState([]);
-
-  // useEffect(() => {
-  //   const fetchAPI = async () => {
-  //     const result = await axios.get(`https://odds.p.rapidapi.com/v4/sports`, {
-  //       params: { all: "true" },
-  //       headers: {
-  //         "x-rapidapi-host": "odds.p.rapidapi.com",
-  //         "x-rapidapi-key": process.env.REACT_APP_RAPID_API_KEY,
-  //       },
-  //     });
-  //     console.log(result.data);
-  //     setItems(result.data);
-  //     setIsLoading(false);
-  //   };
-  //   fetchAPI();
-  // }, []);
 
   return (
     <div>
